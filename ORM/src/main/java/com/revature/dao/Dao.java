ed7d930d47ccd9aaf149d377c400ec50efcca263
xXstrawcakeXx <107396089+xXstrawcakeXx@users.noreply.github.com>
2022-07-10 22:46:26 -0700

    Connection pooling done

---
+++ ORM/src/main/java/com/revature/dao/Dao.java
@@ -0,0 +1,18 @@
+package com.revature.dao;
+
+import java.sql.Connection;
+
+import com.revature.util.Configuration;
+import com.revature.util.MetaModel;
+
+public class Dao {
+	//private static Connection con = Configuration.getConnection();
+
+	//CRUD METHODS
+	
+	public void createTable(MetaModel<?> metaModel) {
+		
+	}
+	
+	
+}
