1d055bfd5c6b2b88375468faca2c82537411cd70
nefrosb <neftali.rosadobermudez@gmail.com>
2022-07-14 07:03:35 -0600

    Quick fixes

--- ORM/src/main/java/com/revature/session/SessionManager.java
+++ ORM/src/main/java/com/revature/session/SessionManager.java
@@ -1,5 +1,23 @@
 package com.revature.session;
 
+import java.sql.SQLException;
+
+import com.revature.util.Configuration;
+
 public class SessionManager {
+	
+	private Configuration cfg;
+	
+	public SessionManager(Configuration cfg) {
+		this.cfg = cfg;
+	}
+		
+	public Session openSession() {
+		return new Session(Configuration.getConnection());
+	}
+	
+	public Session closeSession() {
+		return null;
+	}
 
 }
