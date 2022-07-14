2cd31c49ee4187bd61e2d936ff8b2279ea9338ab
nefrosb <neftali.rosadobermudez@gmail.com>
2022-07-14 07:33:19 -0600

    Changed Hibernate folder so that Superhero works with ORM

--- Hibernate/src/main/java/com/revature/models/Superhero.java
+++ Hibernate/src/main/java/com/revature/models/Superhero.java
@@ -3,47 +3,23 @@ package com.revature.models;
 import java.util.List;
 import java.util.Objects;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 
 
-@Entity
-@Table(name = "Super_Heroes")
+@com.revature.annotations.Entity(tableName = "Super_Heroes")
 public class Superhero {
 	
-	@Id
-	@Column(name = "hero_id")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@com.revature.annotations.Id(columnName = "hero_id")
+	//@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int shId;
 	
-	@Column(name = "hero_name", unique = true, nullable = false)
+	@com.revature.annotations.Column(columnName = "hero_name")
 	private String shName;
 	
-	@Column(name="super_power")
+	@com.revature.annotations.Column( columnName = "super_power")
 	private String superPower;
 	
 	private int rank;
 	
-	// Super Schools should be able to have multiple super Heroes
-	// One super hero can graduate from multiple schools
-	
-	// Fetch type lazy means the data won't be loaded into memory until we call getSuperSchool
-	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY) // Cascade defines the interactions between entities)
-	private List<SuperSchool> superSchool;
-	
-	// Let's set up the interaction between superheroes and their powers
-//	@ManyToOne()
-//	@JoinColumn(name = "super_fk")
-//	private Superpowers superheroHolder; // This is a foreign key that will be mapped to the other column in superpower
 
 	public Superhero() {
 		super();
@@ -54,7 +30,7 @@ public class Superhero {
 		this.shName = shName;
 		this.superPower = superPower;
 		this.rank = rank;
-		this.superSchool = superSchool;
+		
 	}
 
 	public Superhero(int shId, String shName, String superPower, int rank, List<SuperSchool> superSchool) {
@@ -63,7 +39,7 @@ public class Superhero {
 		this.shName = shName;
 		this.superPower = superPower;
 		this.rank = rank;
-		this.superSchool = superSchool;
+		
 	}
 
 	public int getShId() {
@@ -98,17 +74,11 @@ public class Superhero {
 		this.rank = rank;
 	}
 
-	public List<SuperSchool> getSuperSchool() {
-		return superSchool;
-	}
 
-	public void setSuperSchool(List<SuperSchool> superSchool) {
-		this.superSchool = superSchool;
-	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(rank, shId, shName, superPower, superSchool);
+		return Objects.hash(rank, shId, shName, superPower);
 	}
 
 	@Override
@@ -121,13 +91,13 @@ public class Superhero {
 			return false;
 		Superhero other = (Superhero) obj;
 		return rank == other.rank && shId == other.shId && Objects.equals(shName, other.shName)
-				&& Objects.equals(superPower, other.superPower) && Objects.equals(superSchool, other.superSchool);
+				&& Objects.equals(superPower, other.superPower);
 	}
 
 	@Override
 	public String toString() {
 		return "Superhero [shId=" + shId + ", shName=" + shName + ", superPower=" + superPower + ", rank=" + rank
-				+ ", superSchool=" + superSchool + "]";
+				+ "]";
 	}
 	
 
